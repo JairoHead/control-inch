2de15c22ce000efc5ef6960d220d4d4dff7663c0
JairoHead <jairito.gamarra@gmail.com>
2025-08-29 21:00:06 -0500

    Finalize and clean up all database migrations

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: php artisan config:clear && php artisan view:clear && php artisan cache:clear
+start: node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+            \App\Models\User::create([
+            'name' => 'jairo dev',
+            'email' => 'admin@inch.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
 
         $this->call([
             DescargoSeeder::class,
